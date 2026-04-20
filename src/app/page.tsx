bdb9f491acce370adbf952d86638564343411144
Jex02 <jerickivand02@gmail.com>
2026-04-20 22:55:01 +0800

    [Davidon][Initialization] initial Next.js

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <h1 className="text-4xl font-bold">JTIMS Initialized</h1>
+      </div>
+      
+      <div className="mt-10">
+        <p className="text-xl text-gray-600">
+          Jeepney Transit Information Management System - Group 4
+        </p>
+      </div>
+    </main>
+  );
+}
